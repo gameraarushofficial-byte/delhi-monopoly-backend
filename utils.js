e071bae64a7f3007929f271e504b96f278321368
gameraarushofficial-byte <gameraarushofficial@gmail.com>
2026-01-28 15:46:00 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,15 @@
+const crypto = require("crypto");
+
+module.exports = {
+  uuid() {
+    return crypto.randomUUID();
+  },
+
+  rollDice() {
+    return [
+      Math.floor(Math.random() * 6) + 1,
+      Math.floor(Math.random() * 6) + 1
+    ];
+  }
+};
 
